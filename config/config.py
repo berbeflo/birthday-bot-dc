d10d73309a0ad94a957ecaf7f9df1dae3392d7f5
berbeflo <sindhara@outlook.com>
2021-01-10 19:00:25 +0100

    feat(): started with adding the birthday stuff

--- config/config.py
+++ config/config.py
@@ -1,9 +1,12 @@
 from config.group.base import Base
+from config.group.birthday import Birthday
 
 class Config:
     base = None
+    birthday = None
 
     def __init__(self):
         self.base = Base()
+        self.birthday = Birthday()
 
 config = Config()
